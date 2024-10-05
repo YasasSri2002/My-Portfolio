12362f6e662d6a15cb058652964223433fc97e5a
Yasas Medagedara <srimedagedara234@gmail.com>
2024-10-05 19:28:26 +0530

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./componets/header/header.component";
 import { HeroComponent } from './componets/hero/hero.component';
 import * as AOS from 'aos';
+import { AboutMeComponent } from './componets/about-me/about-me.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent,HeroComponent],
+  imports: [RouterOutlet, HeaderComponent,HeroComponent,AboutMeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
