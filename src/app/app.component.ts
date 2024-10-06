b86c3022318c0666bccb5f0730f93b469f7f4f28
Yasas Medagedara <srimedagedara234@gmail.com>
2024-10-06 12:00:13 +0530

    new componet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,12 @@ import { HeaderComponent } from "./componets/header/header.component";
 import { HeroComponent } from './componets/hero/hero.component';
 import * as AOS from 'aos';
 import { AboutMeComponent } from './componets/about-me/about-me.component';
+import { TechStackComponent } from "./componets/tech-stack/tech-stack.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent,HeroComponent,AboutMeComponent],
+  imports: [RouterOutlet, HeaderComponent, HeroComponent, AboutMeComponent, TechStackComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
